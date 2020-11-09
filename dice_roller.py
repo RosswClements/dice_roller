6be5ede6abc6a06dafb7d524f51750399a52984c
RosswClements <RosswClements@hotmail.co.uk>
2020-11-09 14:17:38 +0000

    commenting

--- dice_roller.py
+++ dice_roller.py
@@ -7,11 +7,12 @@ d_type = int(split_string[1])
 rolls = []
 rolls_string = ''
 
+#rolls the chosen dice the specified number of times
 for _ in range(number_of_rolls):
     rolls.append(random.randint(1, d_type))
-
+#joins the rolls into a printable string
 for i in rolls:
     rolls_string += ', '.join(str(i)) + ' '
-
+#prints the sum total of the rolls followed by each individual roll
 print(f"You rolled {sum(rolls)}: {rolls_string}")
     
