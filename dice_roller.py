a4677a73da8b99501926fba375aabac27903b7dc
RosswClements <RosswClements@hotmail.co.uk>
2020-11-09 15:00:30 +0000

    allows for multiple rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,32 @@
 import random
 
-input_string = input("What do you want to roll?: ")
-split_string = input_string.split("d")
-number_of_rolls = int(split_string[0])
-d_type = int(split_string[1])
-rolls = []
-rolls_string = ''
+def main():
 
-#rolls the chosen dice the specified number of times
-for _ in range(number_of_rolls):
-    rolls.append(random.randint(1, d_type))
-#joins the rolls into a printable string
-for i in rolls:
-    rolls_string += ''.join(str(i)) + ' '
-#prints the sum total of the rolls followed by each individual roll
-print(f"You rolled {rolls},{sum(rolls)}: {rolls_string}")
-    
+    continuing = True
+
+    while continuing == True:
+
+        rolls = []
+        rolls_string = ''
+
+        input_string = input("What do you want to roll?: ")  
+        split_string = input_string.split("d")
+        number_of_rolls = int(split_string[0])
+        d_type = int(split_string[1])
+        
+
+        if input_string == 'quit' or input_string == 'Quit':
+            continuing = False
+        else:
+            #rolls the chosen dice the specified number of times
+            for _ in range(number_of_rolls):
+                rolls.append(random.randint(1, d_type))
+            #joins the rolls into a printable string
+            for i in rolls:
+                rolls_string += ''.join(str(i)) + ' '
+            #prints the sum total of the rolls followed by each individual roll
+            print(f"You rolled {sum(rolls)}: {rolls_string}")
+            
+
+main()
+        
